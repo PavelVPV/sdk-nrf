8659653c78de1777f2bc5f5590cac2f031af7105
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2024-09-25 13:46:43 +0200

    DNM: WIP: Made it working through PPIB
    RADIO->PUBLISH_READY set to 4 (fixed channel by mpsl)
    In DPPIC10->CHENSET bit 4 is enabled
    PPIB11->SUBSCRIBE[0] set to 4 (same fixed channel by mpsl)
    PPIB21->PUBLISH[0] set to 4 (same fixed channel by mpsl)
    In DPPIC20->CHENSET bit 4 is enabled
    TIMER->SUBSCRIBE_COUNT set to 4

--- samples/bluetooth/radio_coex_1wire/src/main.c
+++ samples/bluetooth/radio_coex_1wire/src/main.c
@@ -19,6 +19,14 @@
 #include <nrfx_timer.h>
 #include <hal/nrf_gpio.h>
 
+#include <hal/nrf_egu.h>
+#include <nrfx_dppi.h>
+
+#include <hal/nrf_ppib.h>
+
+#include <protocol/mpsl_dppi_protocol_api.h>
+
+#if 0
 /* Generalize PPI or DPPI channel management */
 #if defined(PPI_PRESENT)
 #include <nrfx_ppi.h>
@@ -33,9 +41,10 @@
 #else
 #error "No PPI or DPPI"
 #endif
+#endif
 
 #define APP_COUNTER ((NRF_TIMER_Type *) DT_REG_ADDR(DT_ALIAS(timer)))
-#define APP_COUNTER_RADIO_ACTIVITY_CC 0
+#define APP_COUNTER_RADIO_ACTIVITY_CC 4
 
 #if DT_NODE_HAS_STATUS(DT_PHANDLE(DT_NODELABEL(radio), coex), okay)
 #define COEX_NODE DT_PHANDLE(DT_NODELABEL(radio), coex)
@@ -57,6 +66,14 @@
 
 #define DEVICE_NAME CONFIG_BT_DEVICE_NAME
 #define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+#define PRINT_REGS() do { \
+		printk("%d: RADIO: %x, TIMER10: %x, EGU->S: %x, EGU->P: %x\n", __LINE__, \
+		       *(unsigned int *) 0x5008A300, *(unsigned int *) 0x50085088, \
+		       *(unsigned int *) 0x500C9080, *(unsigned int *) 0x500C9180); \
+	} while (0)
+
+static const nrfx_timer_t app_timer_instance = NRFX_TIMER_INSTANCE(20);
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -84,10 +101,24 @@ static void print_welcome_message(void)
 	printk("-----------------------------------------------------\n");
 }
 
+static bool granted;
+static uint8_t channel = MPSL_DPPI_RADIO_PUBLISH_READY_CHANNEL_IDX;
+
 static void check_input(void)
 {
 	char input_char;
 	int err;
+
+#if 0
+	nrfx_gppi_channel_endpoints_setup(channel,
+		nrf_radio_event_address_get(NRF_RADIO, NRF_RADIO_EVENT_READY),
+#if 1
+		nrf_timer_task_address_get(APP_COUNTER, NRF_TIMER_TASK_COUNT));
+#else
+		nrfx_timer_task_address_get(&app_timer_instance, NRF_TIMER_TASK_COUNT));
+#endif
+#endif
+	PRINT_REGS();
 
 	input_char = console_getchar();
 
@@ -100,6 +131,7 @@ static void check_input(void)
 			printk("Cannot set LED gpio");
 		}
 #endif
+		granted = true;
 		printk("Current state is: Grant every request\n");
 	} else if ((input_char == 'd') ^ APP_GRANT_ACTIVE_LOW) {
 #if 1
@@ -110,49 +142,143 @@ static void check_input(void)
 			printk("Cannot set LED gpio");
 		}
 #endif
+		granted = false;
 		printk("Current state is: Deny every request\n");
 	} else {
 		return;
 	}
+	PRINT_REGS();
 }
+
+static uint32_t egu_counter;
 
 static void console_print_thread(void)
 {
 	while (1) {
+		uint32_t capture = 0;
+
+		PRINT_REGS();
+#if 0
+#if 0
+		do {
+			k_yield();
+			nrf_timer_task_trigger(APP_COUNTER,
+				       nrf_timer_capture_task_get(APP_COUNTER_RADIO_ACTIVITY_CC));
+			capture = nrf_timer_cc_get(APP_COUNTER, APP_COUNTER_RADIO_ACTIVITY_CC);
+		} while (capture == 0 && granted);
+#else
+		for (int i = 0; i < 12; i++) {
+			nrf_timer_task_trigger(APP_COUNTER, NRF_TIMER_TASK_COUNT);
+		}
 		nrf_timer_task_trigger(APP_COUNTER,
 				       nrf_timer_capture_task_get(APP_COUNTER_RADIO_ACTIVITY_CC));
+		capture = nrf_timer_cc_get(APP_COUNTER, APP_COUNTER_RADIO_ACTIVITY_CC);
+#endif
 
-		printk("Number of radio events in the last second: %d\n",
-		       nrf_timer_cc_get(APP_COUNTER, APP_COUNTER_RADIO_ACTIVITY_CC));
+		printk("Egu counter: %d\n", egu_counter);
 
 		nrf_timer_task_trigger(APP_COUNTER, NRF_TIMER_TASK_CLEAR);
+#else
+		capture = nrfx_timer_capture(&app_timer_instance, NRF_TIMER_CC_CHANNEL4);
+		nrfx_timer_clear(&app_timer_instance);
+#endif
+		printk("Number of radio events in the last second: %d\n", capture);
+		printk("DPPIC channels set: %x\n", *(unsigned int *) 0x50082504);
+//		nrfy_dppi_channels_set((NRF_DPPIC_Type *) 0x50082000, NRFX_BIT((uint32_t)channel), true);
+		PRINT_REGS();
 
 		k_sleep(K_MSEC(1000));
 	}
 }
 
-static gppi_channel_t allocate_gppi_channel(void)
+static uint8_t allocate_gppi_channel(void)
 {
-	gppi_channel_t channel;
+	uint8_t channel;
 
-	if (gppi_channel_alloc(&channel) != NRFX_SUCCESS) {
+	if (nrfx_gppi_channel_alloc(&channel) != NRFX_SUCCESS) {
 		__ASSERT(false, "(D)PPI channel allocation error");
 	}
 	return channel;
 }
 
+static void unused_timer_isr_handler(nrf_timer_event_t event_type, void *ctx)
+{
+	ARG_UNUSED(event_type);
+	ARG_UNUSED(ctx);
+}
+
+static void egu_handler(const void *context)
+{
+	egu_counter++;
+}
+
 static void setup_radio_event_counter(void)
 {
+#if 0
 	/* This function sets up a timer as a counter to count radio events. */
+	//nrf_timer_mode_set(APP_COUNTER, NRF_TIMER_MODE_LOW_POWER_COUNTER);
 	nrf_timer_mode_set(APP_COUNTER, NRF_TIMER_MODE_LOW_POWER_COUNTER);
+#else
+	int ret;
+	const nrfx_timer_config_t timer_cfg = {
+		.frequency = NRFX_MHZ_TO_HZ(16UL),
+		.mode = NRF_TIMER_MODE_LOW_POWER_COUNTER,
+		.bit_width = NRF_TIMER_BIT_WIDTH_24,
+		.interrupt_priority = NRFX_TIMER_DEFAULT_CONFIG_IRQ_PRIORITY,
+		.p_context = NULL};
+	ret = nrfx_timer_init(&app_timer_instance, &timer_cfg, unused_timer_isr_handler);
+	if (ret != NRFX_SUCCESS) {
+		printk("Failed initializing timer (ret: %d)\n", ret - NRFX_ERROR_BASE_NUM);
+	}
+#endif
 
-	gppi_channel_t channel = allocate_gppi_channel();
+//	/*uint8_t*/ channel = allocate_gppi_channel();
+//	channel = MPSL_DPPI_RADIO_PUBLISH_READY_CHANNEL_IDX;
+	printk("Channel: %d\n", channel);
 
-	nrfx_gppi_channel_endpoints_setup(
-		channel, nrf_radio_event_address_get(NRF_RADIO, NRF_RADIO_EVENT_READY),
+#if 0
+	nrfx_gppi_fork_endpoint_setup(channel, nrf_timer_task_address_get(APP_COUNTER, NRF_TIMER_TASK_COUNT));
+#elif 0
+	nrf_timer_subscribe_set(APP_COUNTER, NRF_TIMER_TASK_COUNT, channel);
+#elif 0
+	uint8_t dppi_chan_ready;
+	dppi_chan_ready = allocate_gppi_channel();
 
+	printk("dppi_chan_ready %d\n", dppi_chan_ready);
+
+	nrf_egu_subscribe_set(NRF_EGU20, NRF_EGU_TASK_TRIGGER0, channel);
+
+	nrfx_gppi_channel_endpoints_setup(dppi_chan_ready,
+					  nrf_egu_event_address_get(NRF_EGU20, NRF_EGU_EVENT_TRIGGERED0),
 		nrf_timer_task_address_get(APP_COUNTER, NRF_TIMER_TASK_COUNT));
-	gppi_channel_enable(channel);
+
+	nrfx_gppi_channels_enable(BIT(dppi_chan_ready));
+
+	IRQ_DIRECT_CONNECT(EGU20_IRQn, 5, egu_handler, 0);
+	nrf_egu_int_enable(NRF_EGU20, NRF_EGU_INT_TRIGGERED0);
+	NVIC_EnableIRQ(EGU20_IRQn);
+
+#elif 1
+	NRF_DPPI_ENDPOINT_SETUP(nrfx_timer_task_address_get(&app_timer_instance, NRF_TIMER_TASK_COUNT), channel);
+	nrf_ppib_subscribe_set(NRF_PPIB11, NRF_PPIB_TASK_SEND_0, channel);
+	nrf_ppib_publish_set(NRF_PPIB21, NRF_PPIB_EVENT_RECEIVE_0, channel);
+
+#else
+	nrfx_gppi_channel_endpoints_setup(channel,
+		nrf_radio_event_address_get(NRF_RADIO, NRF_RADIO_EVENT_READY),
+#if 0
+		nrf_timer_task_address_get(APP_COUNTER, NRF_TIMER_TASK_COUNT));
+#else
+		nrfx_timer_task_address_get(&app_timer_instance, NRF_TIMER_TASK_COUNT));
+#endif
+#endif
+	nrfx_gppi_channels_enable(BIT(channel));
+	nrfy_dppi_channels_set((NRF_DPPIC_Type *) 0x50082000, NRFX_BIT((uint32_t)channel), true);
+	nrfy_dppi_channels_set((NRF_DPPIC_Type *) 0x500C2000, NRFX_BIT((uint32_t)channel), true);
+	printk("DPPIC channels set: %x\n", *(unsigned int *) 0x50082504);
+	printk("Channel %d is %s\n", channel, nrfx_gppi_channel_check(channel) ? "enabled" : "disabled");
+//	nrfx_timer_enable(&app_timer_instance);
+	PRINT_REGS();
 }
 
 static void setup_grant_pin(void)
@@ -162,9 +288,11 @@ static void setup_grant_pin(void)
 	if (APP_GRANT_ACTIVE_LOW) {
 		nrf_gpio_pin_clear(APP_GRANT_GPIO_PIN);
 		printk("Grant line is set to: %s\n", APP_GRANT_ACTIVE_LOW ? "low" : "high");
+		granted = false;
 	} else {
 		nrf_gpio_pin_set(APP_GRANT_GPIO_PIN);
 		printk("Grant line is set to: %s\n", APP_GRANT_ACTIVE_LOW ? "low" : "high");
+		granted = true;
 	}
 #else
 	int err;
@@ -185,6 +313,9 @@ static void setup_grant_pin(void)
 #endif
 }
 
+#define ADV_NCONN BT_LE_ADV_PARAM(0, BT_GAP_ADV_FAST_INT_MIN_2, \
+					BT_GAP_ADV_FAST_INT_MAX_2, NULL)
+
 int main(void)
 {
 	printk("Starting Radio Coex Demo 1wire on board %s\n", CONFIG_BOARD);
@@ -199,16 +330,20 @@ int main(void)
 	setup_grant_pin();
 	setup_radio_event_counter();
 
-	if (bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), NULL, 0)) {
+	PRINT_REGS();
+	if (bt_le_adv_start(ADV_NCONN, ad, ARRAY_SIZE(ad), NULL, 0)) {
 		printk("Advertising failed to start");
 		return 0;
 	}
+	PRINT_REGS();
 
 	printk("Advertising started\n");
 
 	printk("Grant line is set to: %s\n", APP_GRANT_ACTIVE_LOW ? "low" : "high");
 	printk("Grant pin is set to: %d\n", APP_GRANT_GPIO_PIN);
 	print_welcome_message();
+
+	PRINT_REGS();
 
 	while (1) {
 		k_sleep(K_MSEC(100));
